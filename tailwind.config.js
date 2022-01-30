1d5a712f90f5bc107d117f2102a84b34e1ce7501
João Felipi Cardoso <joaofelipi877@gmail.com>
2022-01-30 11:03:22 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,md,mdx}",
+    "./components/**/*.{js,jsx,md,mdx}",
+    "./pages/*.{js,jsx,md,mdx}",
+    "./pages/index.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  mode: 'jit',
+  plugins: [],
+}
